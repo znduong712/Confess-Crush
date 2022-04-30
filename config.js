e1bde94c6f1187dba03ca3aa7a1e87e030dbcd0e
znduong712 <104667180+znduong712@users.noreply.github.com>
2022-04-30 13:04:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Gửi người tôi yêu",
-    introTitle: '......',
+    introTitle: 'Giang',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
